59120a0c0a4ec2d57b8bb092e40c90b70eab7f83
reyal12 <rheal.rir@gmail.com>
2024-12-11 05:18:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,3 +25,57 @@ void tambahKata(map<string, string>& kamus) {
     kamus[kata] = arti;
     cout << "Kata berhasil ditambahkan!\n";
 }
+// Fungsi untuk mencari arti kata
+void cariKata(const map<string, string>& kamus) {
+    string kata;
+    cout << "\nMasukkan kata yang ingin dicari: ";
+    cin.ignore(); // Membersihkan buffer input
+    getline(cin, kata);
+    auto it = kamus.find(kata);
+    if (it != kamus.end()) {
+        cout << "Arti dari \"" << kata << "\": " << it->second << endl;
+    } else {
+        cout << "Kata tidak ditemukan dalam kamus.\n";
+    }
+}
+
+// Fungsi untuk menampilkan semua kata dalam kamus
+void tampilkanSemuaKata(const map<string, string>& kamus) {
+    if (kamus.empty()) {
+        cout << "\nKamus kosong.\n";
+    } else {
+        cout << "\nDaftar semua kata dalam kamus:\n";
+        for (const auto& pasangan : kamus) {
+            cout << pasangan.first << ": " << pasangan.second << endl;
+        }
+    }
+}
+// Fungsi utama
+int main() {
+    map<string, string> kamus;
+    int pilihan;
+
+    do {
+        tampilkanMenu();
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                tambahKata(kamus);
+                break;
+            case 2:
+                cariKata(kamus);
+                break;
+            case 3:
+                tampilkanSemuaKata(kamus);
+                break;
+            case 4:
+                cout << "Terima kasih telah menggunakan Kamus Mini.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid. Coba lagi.\n";
+        }
+    } while (pilihan != 4);
+
+    return 0;
+}
